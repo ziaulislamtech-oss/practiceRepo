8b63e89f3973937ae25420fd0534a04a4e16ab63
Zia-Ul-Islam <ziaulislam.tech@gmail.com>
2026-03-05 15:23:29 +0500

    practice from zero to get user posts initial stage

--- src/routers/auth.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const authController = require("../controllers/auth.controller")
-
-const authRouter = express.Router()
-
-authRouter.post('/register',authController.registerController)
-authRouter.post('/login',authController.loginController)
-
-module.exports = authRouter
